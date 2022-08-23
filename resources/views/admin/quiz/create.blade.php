4537ff53c69ba64872b52d4d175621e8e5255269
selinduzel <selintuna92@gmail.com>
2022-08-23 21:01:24 +0300

    Video 7

--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -1,45 +1,52 @@
 <x-app-layout>
     <x-slot name="header">Quiz Oluştur</x-slot>
-    <div class="card">
+   <div class="card">
         <div class="card-body">
             <form method="POST" action="{{route('quizzes.store')}}">
                 @csrf
+                
                 <div class="form-group">
-                    <label>Quiz Başlığı </label>
-                    <input type="text" name="title" class="form-control" required>
+                    <label>Quiz Başlığı</label>
+                    <input type="text" name="title" class="form-control" value="{{old('title')}}">
                 </div>
+
                 <div class="form-group">
                     <label>Quiz Açıklama</label>
-                    <textarea name="description" class="form-control" rows="4"></textarea>
-                    <input type="text" name="title" class="form-control" required>
+                    <textarea name="description" class="form-control" rows="4">{{old('description')}}</textarea>
                 </div>
-                <div class="form-group">
-                  <input id=isFinished type="checkbox" >
-                  <label>Bitiş Tarihi Olacak mı?</label>
-                </div>
-                <div id="finishedInput" style="display: none"  class="form-group">
-                    <label>Bitiş Tarihi</label>
-                    <input type="datetime-local" name="finished_at" class="form-control">
-                  </div>
-                <div class="form-group">
-                  <button type ="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
-                  </div>
-            </form>
-     </div>
-</div>
 
-@push('js')
-deneme
+                <div class="form-group">
+                    <input id="isFinished" @if(old('finished_at')) checked @endif type="checkbox">
+                    <label>Bitiş Tarihi Olacak Mı?</label>
+                  </div>
+
+                <div id="finishedInput" @if(!old('finished_at')) style="display:none" @endif class="form-group">
+                    <label>Bitiş Tarihi</label>
+                    <input type="datetime-local" name="finished_at" value='{{old('finished_at')}}' class="form control">
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
+                </div>
+            </form>
+        </div>
+    </div>
+           
+    
+
+    
+<x-slot name="js">
     <script>
         $('#isFinished').change(function(){
          if($('#isFinished').is(':checked')){
            $('#finishedInput').show();
          }else{
           $('#finishedInput').hide();
+         }
         
-          }
         })
     </script>
-@endpush
-
+         
+   
+</x-slot>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,59 +1,81 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-    
-        <title>{{ $header }}-{{ config('app.name', 'Laravel') }}</title>
-    
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <script src="https://kit.fontawesome.com/770ea9a2c1.js" crossorigin="anonymous"></script>
-        <!-- Styles -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        @livewireStyles
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-    
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <title>{{ $header }}-{{ config('app.name', 'Laravel') }}</title>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <script src="https://kit.fontawesome.com/770ea9a2c1.js" crossorigin="anonymous"></script>
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    @livewireStyles
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <x-jet-banner />
+
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation-menu')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                         {{ $header }}
-                        </h2>
-                    </div>
-                </header>
-            @endif
-
-            
-    
-            <div class="py-6">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                 {{ $slot }}
-                 @stack('js')
+                    </h2>
                 </div>
+            </header>
+        @endif
+
+
+
+        <div class="py-6">
+
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
+
+
+                @if(session('success'))
+                      <div class="alrt alert-success">
+                      <i class="fa fa-check"><i/>
+                       {{session('success')}}
+                      </div>
+                @endif
+
+                {{ $slot }}
             </div>
         </div>
-                
-            
-        </div>
 
-        @stack('modals')
 
+    </div>
+
+    @stack('modals')
+    @isset($js)
+        {{ $js }}
+        @endif
         @livewireScripts
     </body>
-</html>
+
+    </html>
