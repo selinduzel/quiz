cb406f290b5716daeee239c0166eb5d1e63099fc
selinduzel <selintuna92@gmail.com>
2022-08-25 14:09:29 +0300

    Video 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\QuizController;
+use App\Http\Controllers\Admin\QuestionController;
 
 
 Route::get('/', function () {
@@ -21,10 +22,10 @@ Route::middleware([
 
 
  
-Route::group(['middleware' => ['auth','isAdmin'],'prefix' => 'admin'],function() {
-     
-Route::get('quizzes/{id}',[QuizController::class,'destroy'])->whereNumber('id')->name('quizzes.destory');
+    Route::group(['middleware' => ['auth','isAdmin'],'prefix' => 'admin'],function() {
+    Route::get('quizzes/{id}',[QuizController::class,'destroy'])->whereNumber('id')->name('quizzes.destory');
+    Route::resource('quizzes',QuizController::class);
+    Route::resource('quiz/{quiz_id}/questions',QuestionController::class);
 
-Route::resource('quizzes',QuizController::class);
 
- });
+ }); 
