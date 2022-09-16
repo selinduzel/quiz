e636569997638e1e8792edb478e2a501ea494c32
selinduzel <selintuna92@gmail.com>
2022-09-17 01:25:58 +0300

    Video 25

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,8 @@
         <div class="col-md-8">
             <div class="list-group">
                 @foreach ($quizzes as $quiz)
-                    <a href="{{route('quiz.detail',$quiz->slug)}}" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <a href="{{ route('quiz.detail', $quiz->slug) }}"
+                        class="list-group-item list-group-item-action flex-column align-items-start">
                         <div class="d-flex w-100 justify-content-between">
                             <h5 class="mb-1">{{ $quiz->title }}</h5>
                             <small>{{ $quiz->finished_at ? $quiz->finished_at->diffForHumans() . ' bitiyor.' : null }}</small>
@@ -20,7 +21,21 @@
             </div>
         </div>
         <div class="col-md-4">
-            deneme
+            <div class="card">
+                <div class="card-header">
+                    Quiz Sonuçları
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach ($results as $result)
+                        <li class="list-group-item">
+                            <strong>{{ $result->point }}</strong> -
+                            <a href="{{route('quiz.detail',$result->quiz->slug)}}">
+                            {{ $result->quiz->title }}
+                        </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 
